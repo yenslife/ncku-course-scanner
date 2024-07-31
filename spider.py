787619bc15a106a583e3a643e25b445c7c12d6c3
海狸大師 <86974027+yenslife@users.noreply.github.com>
2024-07-31 22:28:48 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -149,10 +149,11 @@ for account in waiting_list:
     for course in data:
         for query_course in course_preferences:
             if is_ids(query_course) and query_course in course['系號-序號']:
-                query_result.append(course)
+                query_result.append(query_course)
                 continue
             if query_course in course['科目名稱']:
-                query_result.append(course)
+                query_result.append(query_course)
+              
     # if ✅ sending email
     md_result = markdown_table_format(query_result)
     if '✅' in md_result:
